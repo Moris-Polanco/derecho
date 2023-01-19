eb068f87d7f01bd572f9539d93a1e37127264bb0
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-01-19 04:52:56 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import openai
 import streamlit as st
+import os
 
-openai.api_key = "YOUR_API_KEY"
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 def analyze_case():
     case_info = st.text_input("Enter the information about the legal case you want to analyze")
