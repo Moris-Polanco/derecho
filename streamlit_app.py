5f05e7bc28646615d32586a5996ee8c00e41e134
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-01-19 05:23:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,25 +5,38 @@ import os
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 
-
-def analizar_caso():
-    informacion_caso = st.text_input("Ingrese la información sobre el caso legal que desea analizar")
-    if st.button("Analizar"):
-        respuesta = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=f'Analizar un caso legal de acuerdo con la legislación guatemalteca, incluyendo la cita de la ley violada y la pena merecida. Si no se puede citar la ley, indicar que el análisis no se puede realizar. {informacion_caso}',
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            temperature=0.7,
-        )
-        if 'no se puede citar la ley' in respuesta["choices"][0]["text"]:
-            st.warning(respuesta["choices"][0]["text"])
+def analyze_case():
+    case_info = st.text_input("Enter the information about the legal case you want to analyze")
+    document1 = st.file_uploader("Upload first document")
+    document2 = st.file_uploader("Upload second document")
+    document3 = st.file_uploader("Upload third document")
+    if st.button("Analyze"):
+        if document1 and document2 and document3:
+            try:
+                doc1 = open(document1, 'r').read()
+                doc2 = open(document2, 'r').read()
+                doc3 = open(document3, 'r').read()
+            except:
+                st.error("Some error occurred while reading the document")
+                return
+            prompt = f'Analyze a legal case according to Guatemalan legislation using the following documents: {doc1}, {doc2}, {doc3}. {case_info}'
+            response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt=prompt,
+                max_tokens=1024,
+                n=1,
+                stop=None,
+                temperature=0.7,
+            )
+            if 'unable to cite the law' in response["choices"][0]["text"]:
+                st.warning(response["choices"][0]["text"])
+            else:
+                st.success(response["choices"][0]["text"])
+            if st.button("Success"):
+                st.success("The case has been successfully analyzed")
         else:
-            st.success(respuesta["choices"][0]["text"])
-        if st.button("Exito"):
-            st.success("El caso ha sido analizado con éxito")
+            st.error("Please upload all the required documents")
 
-st.title("Analizador de casos legales")
-st.write("Ingrese información sobre el caso legal que desea analizar y presione el botón 'Analizar'")
-analizar_caso()
+st.title("Legal case analyzer")
+st.write("Enter information about the legal case you want to analyze and upload the required documents, then press the 'Analyze' button")
+analyze_case()
