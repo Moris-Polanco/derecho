b33bbb7f7ea88a4e6f391f58fe2d3ce222577edb
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-01-19 04:51:02 -0600

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,23 @@
+import openai
+import streamlit as st
+
+openai.api_key = "YOUR_API_KEY"
+
+def analyze_case():
+    case_info = st.text_input("Enter the information about the legal case you want to analyze")
+    if st.button("Analyze"):
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=f'Analyze a legal case according to Guatemalan legislation {case_info}',
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.7,
+        )
+        st.success(response["choices"][0]["text"])
+        if st.button("Success"):
+            st.success("The case has been successfully analyzed")
+
+st.title("Legal case analyzer")
+st.write("Enter information about the legal case you want to analyze and press the 'Analyze' button")
+analyze_case()
