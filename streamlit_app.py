fdd24002365aa113d1b8232d096fb2acc88acc8f
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-01-19 05:17:47 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,24 +5,25 @@ import os
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 
-def analyze_case():
-    case_info = st.text_input("Enter the information about the legal case you want to analyze")
-    if st.button("Analyze"):
-        response = openai.Completion.create(
+
+def analizar_caso():
+    informacion_caso = st.text_input("Ingrese la información sobre el caso legal que desea analizar")
+    if st.button("Analizar"):
+        respuesta = openai.Completion.create(
             engine="text-davinci-002",
-            prompt=f'Analyze a legal case according to Guatemalan legislation including the citation of the law that is violated and the penalty that is deserved. If unable to cite the law, state that the analysis can not be done. {case_info}',
+            prompt=f'Analizar un caso legal de acuerdo con la legislación guatemalteca, incluyendo la cita de la ley violada y la pena merecida. Si no se puede citar la ley, indicar que el análisis no se puede realizar. {informacion_caso}',
             max_tokens=1024,
             n=1,
             stop=None,
             temperature=0.7,
         )
-        if 'unable to cite the law' in response["choices"][0]["text"]:
-            st.warning(response["choices"][0]["text"])
+        if 'no se puede citar la ley' in respuesta["choices"][0]["text"]:
+            st.warning(respuesta["choices"][0]["text"])
         else:
-            st.success(response["choices"][0]["text"])
-        if st.button("Success"):
-            st.success("The case has been successfully analyzed")
+            st.success(respuesta["choices"][0]["text"])
+        if st.button("Exito"):
+            st.success("El caso ha sido analizado con éxito")
 
-st.title("Legal case analyzer")
-st.write("Enter information about the legal case you want to analyze and press the 'Analyze' button")
-analyze_case()
+st.title("Analizador de casos legales")
+st.write("Ingrese información sobre el caso legal que desea analizar y presione el botón 'Analizar'")
+analizar_caso()
